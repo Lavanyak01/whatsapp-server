6ffad5ca90b4988b15a21f901c729df1e5a1b8fb
Lavanyak01 <102778119+Lavanyak01@users.noreply.github.com>
2023-01-10 13:02:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const server = app.listen(PORT, ()=>{
 const io = require('socket.io')(server,{
     pingTimeout: 60000,
     cors: {
-        origin: ['http://localhost:3000', 'https://whatsapp-client.onrender.com']
+        origin: ['http://localhost:3000', 'https://whatsapp-8q1e.onrender.com']
         
     },
 });
